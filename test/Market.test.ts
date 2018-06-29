3b42eaf95ce32c9f561375889fa43243402a124e
Phil Elsasser <phil@marketprotocol.io>
2018-06-29 14:39:52 -0600

    update all constructors

--- test/Market.test.ts
+++ test/Market.test.ts
@@ -1,13 +1,17 @@
 import Web3 from 'web3';
 
 import { Market } from '../src/';
+import { MARKETProtocolConfig } from '../src/types/Configs';
 
 /**
  * Market
  */
 describe('Market class', () => {
   it('Market is instantiable', () => {
-    const market = new Market(new Web3.providers.HttpProvider('http://localhost:9545'));
+    const config: MARKETProtocolConfig = {
+      networkId: 4447
+    };
+    const market = new Market(new Web3.providers.HttpProvider('http://localhost:9545'), config);
     expect(market).toBeInstanceOf(Market);
   });
 });
