e73838a67a2a04270135f494af0547dfb61d6d1d
Phil Elsasser <phil@marketprotocol.io>
2018-07-12 17:34:36 -0600

    add ability to import from abi repo

--- src/artifacts.ts
+++ src/artifacts.ts
@@ -1,11 +1,31 @@
 import { Artifact } from '@marketprotocol/types';
 
-const OrderLibArtifact = require('../build/contracts/OrderLib.json');
-const MathLibArtifact = require('../build/contracts/MathLib.json');
-const MarketTokenArtifact = require('../build/contracts/MarketToken.json');
-const MarketContractRegistryArtifact = require('../build/contracts/MarketContractRegistry.json');
-const MarketContractFactoryOraclize = require('../build/contracts/MarketContractFactoryOraclize.json');
-const MarketCollateralPoolFactory = require('../build/contracts/MarketCollateralPoolFactory.json');
+let OrderLibArtifact;
+let MathLibArtifact;
+let MarketTokenArtifact;
+let MarketContractRegistryArtifact;
+let MarketContractFactoryOraclize;
+let MarketCollateralPoolFactory;
+
+// TODO: this is pretty hacky, but does work.  Need a more elegant solution
+
+try {
+  // attempt to import from local build
+  OrderLibArtifact = require('../build/contracts/OrderLib.json');
+  MathLibArtifact = require('../build/contracts/MathLib.json');
+  MarketTokenArtifact = require('../build/contracts/MarketToken.json');
+  MarketContractRegistryArtifact = require('../build/contracts/MarketContractRegistry.json');
+  MarketContractFactoryOraclize = require('../build/contracts/MarketContractFactoryOraclize.json');
+  MarketCollateralPoolFactory = require('../build/contracts/MarketCollateralPoolFactory.json');
+} catch (e) {
+  // if not found, use artifacts from abis repo
+  OrderLibArtifact = require('@marketprotocol/abis/build/contracts/OrderLib.json');
+  MathLibArtifact = require('@marketprotocol/abis/build/contracts/MathLib.json');
+  MarketTokenArtifact = require('@marketprotocol/abis/build/contracts/MarketToken.json');
+  MarketContractRegistryArtifact = require('@marketprotocol/abis/build/contracts/MarketContractRegistry.json');
+  MarketContractFactoryOraclize = require('@marketprotocol/abis/build/contracts/MarketContractFactoryOraclize.json');
+  MarketCollateralPoolFactory = require('@marketprotocol/abis/build/contracts/MarketCollateralPoolFactory.json');
+}
 
 export const artifacts = {
   OrderLibArtifact: OrderLibArtifact as Artifact,
